92377fbead9db600507f1b752db4f2a232fd4774
Ammon Morris <likesalmon@gmail.com>
2015-04-15 11:18:07 -0700

    added tests for buttons

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -17,10 +17,19 @@ describe('my app', function() {
             browser.get('index.html#/shuffler');
         });
 
-
         it('should render shuffler.html when user navigates to /shuffler', function() {
             expect(element.all(by.css('[ng-view] h1')).first().getText()).
             toMatch(/Shuffler/);
+        });
+
+        describe('shuffle button', function () {
+            var shuffleButton = element(by.id('shuffle-button'));
+            expect(shuffleButton.isPresent()).toBe(true);
+        });
+
+        describe('sort button', function () {
+            var sortButton = element(by.id('shuffle-button'));
+            expect(sortButton.isPresent()).toBe(true);
         });
 
         describe('cards', function () {
@@ -28,6 +37,20 @@ describe('my app', function() {
 
             it('should have 52 cards', function () {
                 expect(cards.count()).toBe(52);
+            });
+
+            it('should be ordered by clubs, diamonds, hearts, spades', function () {
+                cards.all(by.css('.suit')).each(function (el, i) {
+                    var suit = el.getAttribute('suit');
+
+                    if (i < 13) expect(suit).toBe('clubs');
+
+                    if (i > 12 && i < 26) expect(suit).toBe('diamonds');
+
+                    if (i > 25 && i < 39) expect(suit).toBe('hearts');
+
+                    if (i > 38) expect(suit).toBe('spades');
+                });
             });
         });
 
