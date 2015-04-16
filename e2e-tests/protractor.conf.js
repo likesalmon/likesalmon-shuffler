286d56241594e71b4f442cf9079813d5ea312213
Ammon Morris <likesalmon@gmail.com>
2015-04-16 09:56:36 -0700

    upgraded framework to jasmine2; using jasmine-spec-reporter

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -13,9 +13,8 @@ exports.config = {
 
     baseUrl: 'http://localhost:8000/app/',
 
-    framework: 'jasmine',
+    framework: 'jasmine2',
 
-    /* does not work with jasmine < 2.0 */
     onPrepare: function() {
         var SpecReporter = require('jasmine-spec-reporter');
         jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: true}));
