82bfc1cadca7a15436a3b3d3f5674d0c9498ea7d
Ammon Morris <likesalmon@gmail.com>
2015-04-16 09:36:04 -0700

    refactored using promises

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* https://github.com/angular/protractor/blob/master/docs/toc.md */
+var _ = require('lodash');
 
 describe('my app', function() {
 
@@ -12,45 +12,82 @@ describe('my app', function() {
 
 
     describe('shuffler', function() {
+        var sortButton,
+            shuffleButton,
+            cards;
 
         beforeEach(function() {
             browser.get('index.html#/shuffler');
+            shuffleButton = element(by.id('shuffle-button'));
+            sortButton = element(by.id('sort-button'));
+            cards = element.all(by.repeater('card in deck'));
         });
 
         it('should render shuffler.html when user navigates to /shuffler', function() {
-            expect(element.all(by.css('[ng-view] h1')).first().getText()).
-            toMatch(/Shuffler/);
+            element.all(by.css('[ng-view] h1')).first().getText().then(function (text) {
+                expect(text).toMatch(/Shuffler/);
+            });
         });
 
         describe('shuffle button', function () {
-            var shuffleButton = element(by.id('shuffle-button'));
-            expect(shuffleButton.isPresent()).toBe(true);
+            it('should exist', function () {
+                shuffleButton.isPresent().then(function (isPresent) {
+                    expect(isPresent).toBe(true);
+                });
+            });
         });
 
         describe('sort button', function () {
-            var sortButton = element(by.id('shuffle-button'));
-            expect(sortButton.isPresent()).toBe(true);
+            it('should exist', function () {
+                sortButton.isPresent().then(function (isPresent) {
+                    expect(isPresent).toBe(true);
+                });
+            });
         });
 
         describe('cards', function () {
-            var cards = element.all(by.repeater('card in deck'));
-
-            it('should have 52 cards', function () {
-                expect(cards.count()).toBe(52);
+            it('should exist', function () {
+                cards.count().then(function (count) {
+                    expect(count).toBe(52);
+                });
             });
 
             it('should be ordered by clubs, diamonds, hearts, spades', function () {
                 cards.all(by.css('.suit')).each(function (el, i) {
-                    var suit = el.getAttribute('suit');
+                    var suit = el.getAttribute('suit').then(function (suit) {
+                        if (i < 13) expect(suit).toBe('clubs');
 
-                    if (i < 13) expect(suit).toBe('clubs');
+                        if (i > 12 && i < 26) expect(suit).toBe('diamonds');
 
-                    if (i > 12 && i < 26) expect(suit).toBe('diamonds');
+                        if (i > 25 && i < 39) expect(suit).toBe('hearts');
 
-                    if (i > 25 && i < 39) expect(suit).toBe('hearts');
-
-                    if (i > 38) expect(suit).toBe('spades');
+                        if (i > 38) expect(suit).toBe('spades');
+                    });
                 });
+            });
+
+            it('should change sort order when shuffle button is clicked', function () {
+                var dealt,
+                    shuffled,
+                    sorted;
+
+                element.all(by.css('.suit')).getAttribute('suit').then(function (suits) {
+                        dealt = suits;
+
+                        shuffleButton.click();
+
+                        element.all(by.css('.suit')).getAttribute('suit').then(function(suits) {
+                            shuffled = suits;
+                            expect(_.isEqual(dealt, sorted)).toBe(false);
+
+                            sortButton.click();
+
+                            element.all(by.css('.suit')).getAttribute('suit').then(function(suits) {
+                                sorted = suits;
+                                expect(_.isEqual(dealt, sorted)).toBe(true);
+                            });
+                        });
+                    });
             });
         });
 
